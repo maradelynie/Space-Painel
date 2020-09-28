2adfb4bfef9326d31c9420273cc84862e55e0bfc
maradelynie <32828701+maradelynie@users.noreply.github.com>
2020-09-28 14:59:22 -0300

    correction on background effect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
-import Background from "./components/background";
 import "./scss/global.scss";
 
 import { Provider } from "react-redux";
@@ -10,7 +9,6 @@ import Store from "./redux/storage";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={Store}>
-      <Background />
       <div className="main_container">
         <h1 className="spacepainel_title">Space Painel</h1>
         <div className="card_container">
